7cdcef9c07fa4824f1401939bc3e73d9a8f934ec
John Boynton <jcboynton76@yahoo.com>
2018-04-13 02:24:36 -0400

    Working starup

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+app_home=/opt/sites/helloworld
+
+mkdir $app_home -p
+chown ec2-user:ec2-user $app_home
+
+yum -y update
 yum install -y nodejs npm --enablerepo=epel
+yum -y groupinstall development
+
+cd $app_home/helloworld
 npm config set strict-ssl false
 npm install
